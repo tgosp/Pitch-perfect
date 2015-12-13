d09899723043bef6b5aafecd1120efd8db076fbf
Todor Gospodinov <gospodinov.web@gmail.com>
2015-12-13 18:06:15 +0200

    second view linked to play sounds view controller

---
+++ PitchPerfect/PlaySoundsViewControlller.swift
@@ -0,0 +1,21 @@
+//
+//  PlaySoundsViewControlller.swift
+//  PitchPerfect
+//
+//  Created by Todor Gospodinov on 12/13/15.
+//  Copyright © 2015 Todor Gospodinov. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewControlller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+ 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
