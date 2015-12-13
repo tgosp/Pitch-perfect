5d06115a3220b7ce39a23a4e1fcec0142768622a
Todor Gospodinov <gospodinov.web@gmail.com>
2015-12-13 22:44:27 +0200

    1st effect done

--- PitchPerfect/PlaySoundsViewControlller.swift
+++ PitchPerfect/PlaySoundsViewControlller.swift
@@ -15,7 +15,8 @@ class PlaySoundsViewControlller: UIViewController, AVAudioPlayerDelegate {
     
     var audioPlayer:AVAudioPlayer!
     var recievedAudio:RecordedAudio!
-
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +24,11 @@ class PlaySoundsViewControlller: UIViewController, AVAudioPlayerDelegate {
         // MARK: create audio player
         audioPlayer = try! AVAudioPlayer(contentsOfURL: recievedAudio.filePathUrl)
         audioPlayer.enableRate = true
+        
         audioPlayer.delegate = self
-
+        
+        audioEngine = AVAudioEngine()
+        audioFile = try! AVAudioFile(forReading: recievedAudio.filePathUrl)
     }
 
     
@@ -53,6 +57,34 @@ class PlaySoundsViewControlller: UIViewController, AVAudioPlayerDelegate {
         audioPlayer.rate = 2
         audioPlayer.play()
     }
+    
+    @IBAction func playChipmunkAudio(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
+    }
+    
+    func playAudioWithVariablePitch(pitch: Float){
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        let audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        let changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        try! audioEngine.start()
+        
+        audioPlayerNode.play()
+        
+    }
+    
     
     @IBAction func stopPlayAudio(sender: UIButton) {
         // MARK: stop play audio
